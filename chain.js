fdffb05c4111d678a01d03d4961489f1863b18f5
Homletmoo <samlhub@gmail.com>
2016-01-31 12:21:10 +0000

    Remove more bad language.

--- chain.js
+++ chain.js
@@ -10,13 +10,11 @@ var TITLES = [
   'Think again, dweeb.',
   'As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot.',
   'The storm that wipes out the pathetic little thing you call your life.',
-  'You’re dead, kid.',
   'I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands.',
   'Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable self off the face of the continent.',
   'If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your tongue.',
-  'But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot.',
+  'But you couldn’t, you didn’t, and now you’re paying the price, you fool.',
   'I will rain fury all over you and you will drown in it.',
-  'You’re dead, kiddo.',
 
   'I don’t care who you are or where you live.',
   'You can count on me to be there to bring your life to a hellish end.',
@@ -39,13 +37,11 @@ var TITLES = [
   'Think again, noob.',
   'As we speak this copypasta is contacting it’s secret network of redditors across the USA and your IP is being doxxed right now so you better prepare for the storm, maggot.',
   'The storm that wipes out the pathetic little thing you call your life.',
-  'You’re dead, kid.',
   'Navy Seal copypastas can be anywhere, anytime, and they can confuse you in over seven hundred ways, and that’s just with mad-lib permutations.',
-  'Not only are they extensively trained in trolling, but they have access to the entire arsenal of Anonymous and will use it to its full extent to wipe your miserable ass off the face of the Internet, you little noob.',
+  'Not only are they extensively trained in trolling, but they have access to the entire arsenal of Anonymous and will use it to its full extent to wipe you off the face of the Internet, you little noob.',
   'If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your tongue.',
-  'But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot.',
+  'But you couldn’t, you didn’t, and now you’re paying the price, you fool.',
   'This copypasta will rain fury all over you and you will drown in it.',
-  'You’re dead, kiddo.',
 
   'What in the name of the Queen did you just say about me, you little chav?',
   'I’ll have you know I graduated top of my class in the SAS, and I’ve been involved in numerous secret raids on Ireland, and I have over 300 confirmed kills.',
@@ -56,13 +52,11 @@ var TITLES = [
   'Think again, chap.',
   'As we speak I am contacting my secret network of spies across the world and your IP is being traced right now so you better prepare for the storm, banger.',
   'The storm that wipes out the pathetic little thing you call your life.',
-  'You’re dead, kid.',
   'I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands.',
-  'Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the Royal Marines and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little muppet.',
+  'Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the Royal Marines and I will use it to its full extent to wipe you off the face of the continent, you little muppet.',
   'If only you could have known what unholy retribution your little "clever" comment was about to bring down upon you, maybe you would have held your tongue.',
   'But you couldn’t, you didn’t, and now you’re paying the price, you dolt.',
   'I will rain fury all over you and you will drown in it.',
-  'You’re dead, mate.'
 ];
 
 var terms = [];
